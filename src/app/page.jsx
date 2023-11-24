5866f07aa588973d46122d32683a7ce297deb742
JudisP <judisson429@hotmail.com>
2023-11-24 16:15:39 -0300

    Refactor - Mudando de .js para .jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,16 +2,19 @@ import React from "react";
 
 const page = () => {
   return (
-    <>
-      <h1>page</h1>
-      <p>
+    <div className="">
+      <h1 className="text-2xl">Home</h1>
+      {/* <p className="text-7x1">
+        texto
+      </p>
+      <p className="text-5x1">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nunc
         ipsum, efficitur ut commodo sed, facilisis tempor ipsum. Donec volutpat,
         nibh eu gravida rutrum, sapien odio efficitur sem, et tempus neque dui
         non nisi. Phasellus sed nulla sed leo rhoncus auctor quis vel orci.
         Pellentesque mollis neque nec ex ullamcorper.
-      </p>
-    </>
+      </p> */}
+    </div>
   );
 };
 
